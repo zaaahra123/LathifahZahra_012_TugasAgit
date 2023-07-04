1485efe8b5035dff15e83351ad5c4b06bf6df8bb
student <student@UPTIF-140>
2023-07-04 08:54:05 +0700

    Yang baru

---
+++ Lathifah Zahratul Janah_012_Asinkronus_WPF/agit-polman-2023-asinkronus-multithread-master/agit-polman-2023-asinkronus-multithread-master/Asinkronus Multithread/Asinkronus.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asinkronus_Multithread
+{
+    internal class Asinkronus
+    {
+        public static async Task FirstAsync()
+        {
+            //Console.WriteLine("First Async Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(4000);
+            Console.WriteLine("First Async Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        public static async Task SecondAsync()
+        {
+            //Console.WriteLine("Second Async Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(2000);
+            Console.WriteLine("Second Async Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        public static async Task ThirdAsync()
+        {
+            //Console.WriteLine("Third Async Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(1000);
+            Console.WriteLine("Third Async Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+        }
+        public static async Task FourAsync()
+        {
+            //Console.WriteLine("First Async Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(6000);
+            Console.WriteLine("Nama : Saya Zahra | Id : " + Thread.CurrentThread.ManagedThreadId);
+        }
+        public static async Task FiveAsync()
+        {
+            //Console.WriteLine("First Async Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(5000);
+            Console.WriteLine("Prodi : Manajemen Informatika | Id : " + Thread.CurrentThread.ManagedThreadId);
+        }
+        public static async Task ExecuteAsyncFunctions()
+        {
+            var firstAsync = FirstAsync();
+            var secondAsync = SecondAsync();
+            var thirdAsync = ThirdAsync();
+            var fourAsync = FourAsync();
+            var fiveAsync = FiveAsync();
+            await Task.WhenAll(firstAsync, secondAsync, thirdAsync, fourAsync, fiveAsync);
+        }
+        
+    }
+}
---
+++ Lathifah Zahratul Janah_012_Asinkronus_WPF/agit-polman-2023-asinkronus-multithread-master/agit-polman-2023-asinkronus-multithread-master/Asinkronus Multithread/Multithreading.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asinkronus_Multithread
+{
+    internal class Multithreading
+    {
+        public static void FirstMethod()
+        {
+            Console.WriteLine("First Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(10000);
+            Console.WriteLine("First Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        public static void SecondMethod()
+        {
+            Console.WriteLine("Second Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(9000);
+            Console.WriteLine("Second Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        public static void ThirdMethod()
+        {
+            Console.WriteLine("Third Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(8000);
+            Console.WriteLine("Third Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+        }
+        public static void FourMethod()
+        {
+            Console.WriteLine("Nama : Zahra with Id: " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(7000);
+        }
+        public static void FiveMethod()
+        {
+            Console.WriteLine("Prodi : Manajemen Informatika with Id: " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(6000);
+        }
+        public static void ExecuteMultithreading()
+        {
+            Thread t1 = new Thread(new ThreadStart(FirstMethod));
+            Thread t2 = new Thread(new ThreadStart(SecondMethod));
+            Thread t3 = new Thread(new ThreadStart(ThirdMethod));
+            Thread t4 = new Thread(new ThreadStart(FourMethod));
+            Thread t5 = new Thread(new ThreadStart(FiveMethod));
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+            t5.Start();
+        }
+    }
+}
